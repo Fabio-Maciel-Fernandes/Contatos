57ff46fe448c5b7bf22b88dd1bb0dd8f1c6a4cb1
Fabio Maciel Fernandes <fabbao@gmail.com>
2025-02-06 18:53:19 -0300

    Autoscaling

--- Contatos.Api/Program.cs
+++ Contatos.Api/Program.cs
@@ -13,7 +13,7 @@ using Microsoft.ApplicationInsights.AspNetCore;
 [ExcludeFromCodeCoverage]
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main(string[] args) 
     {
 
         var builder = WebApplication.CreateBuilder(args);
